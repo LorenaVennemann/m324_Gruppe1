fef27f7cfa55db77cdb2b7e9e47a91dbdc8891ef
Sandro <sandro.kohler@edu.tbz.ch>
2025-09-01 11:46:56 +0200

    add files

---
+++ Code/airport-service/src/main/java/tbz/ch/fluege/FluegeApplication.java
@@ -0,0 +1,13 @@
+package tbz.ch.fluege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluegeApplication.class, args);
+	}
+
+}
---
+++ Code/flight-service/src/main/java/tbz/ch/fluege/FlightServiceApplication.java
@@ -0,0 +1,13 @@
+package tbz.ch.fluege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluegeApplication.class, args);
+	}
+
+}
