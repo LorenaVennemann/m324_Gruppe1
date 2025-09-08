343fcd2fde190b16de4df5fc52ca869511c800ea
Sandro <sandro.kohler@edu.tbz.ch>
2025-09-08 09:13:31 +0200

    add airport entity

--- Code/airport-service/src/main/java/tbz/ch/airport/entity/Airport.java
+++ Code/airport-service/src/main/java/tbz/ch/airport/entity/Airport.java
@@ -1,4 +1,24 @@
 package tbz.ch.airport.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "code")
 public class Airport {
+    @Id
+    private int id;
+    private String name;
+    private String code;
+    private Integer capacity;
+    private LocalDateTime createdAt;
 }
