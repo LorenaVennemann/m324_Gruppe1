74bbda8fd45c907f75cd300254eabfaa9bd7d4af
LorenaVennemann <jil.vennemann@gmail.com>
2025-09-29 09:58:45 +0200

    units test

--- Code/flight-service/src/main/java/tbz/ch/flight/dto/FlightRequest.java
+++ Code/flight-service/src/main/java/tbz/ch/flight/dto/FlightRequest.java
@@ -1,18 +1,36 @@
 package tbz.ch.flight.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
 public class FlightRequest {
-
     private String departureAirportCode;
     private String arrivalAirportCode;
     private LocalDateTime departureDatetime;
     private LocalDateTime arrivalDatetime;
     private String aircraftType;
+
+
+    public String getDepartureAirportCode() {
+        return departureAirportCode;
+    }
+
+    public String getArrivalAirportCode() {
+        return arrivalAirportCode;
+    }
+
+    public LocalDateTime getDepartureDatetime() {
+        return departureDatetime;
+    }
+
+    public LocalDateTime getArrivalDatetime() {
+        return arrivalDatetime;
+    }
+
+    public String getAircraftType() {
+        return aircraftType;
+    }
 }
